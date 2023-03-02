0f2982c0df6edebe775bfdeeb5aeb715ea6b06aa
keiko233 <keiko23333@gmail.com>
2023-03-02 09:13:20 +0800

    refactor: plugin script

--- src/gerrit-plugin/index.ts
+++ src/gerrit-plugin/index.ts
@@ -6,30 +6,24 @@ import ToolsView from '../components/vue/ToolsView.vue'
 
 printCard('Mia-Theme Vue Plugin by keiko233 & ReallySnow');
 
-const headerHTMl = `<template>
-  <style>
-    html {
-      --header-background-color: var(--theme-primary);
-      --header-text-color: #fff;
-      --header-title-content: "Project Mia";
-      --header-padding: 6px;
-      --header-box-shadow: var(--theme-primary-shadow);
-      --border-radius: var(--border-radius);
-    }
-  </style>
-</template>`;
-
 if (isGerritEnvironment()) {
-  console.log('Is Gerrit environment, inject Register.')
+  console.log('Is Gerrit environment, inject Register.');
+
+  const headerHTMl = `<template>
+    <style>
+      html {
+        --header-background-color: var(--theme-primary);
+        --header-text-color: #fff;
+        --header-title-content: "Project Mia";
+        --header-padding: 6px;
+        --header-box-shadow: var(--theme-primary-shadow);
+        --border-radius: var(--border-radius);
+      }
+    </style>
+  </template>`;
   injectionRegister(headerHTMl);
-} else {
-  console.log('Not Gerrit environment, skip inject.')
-}
 
-
-if (isGerritEnvironment()) {
   const gerritMainHeader = getGerritMainHeader();
-
   gerritMainHeader!.querySelector("gr-endpoint-decorator")!.innerHTML = `
   <div style="min-width:120px;font-family:var(--font-family,&quot;&quot;),&quot;Roboto&quot;,Arial,sans-serif;font-weight:400;line-height:20px;"></div>
     <div style="font-size:20px">
@@ -52,9 +46,9 @@ if (isGerritEnvironment()) {
     overflow: initial !important;
   }`;
   gerritMainHeader?.appendChild(insertMainHeaderStyle);
-}
 
-if (isGerritEnvironment()) {
   const vueLoader = document.body.appendChild(document.createElement('div'));
   createApp(h(ToolsView)).mount(vueLoader);
+} else {
+  console.log('Not Gerrit environment, skip inject.')
 }
