6e3c707301c531f059f675ba8c7b696db001d953
keiko233 <keiko23333@gmail.com>
2023-03-02 17:28:57 +0800

    feat: add insertElementStyleById func

--- src/components/ts/StyleInsert.ts
+++ src/components/ts/StyleInsert.ts
@@ -10,3 +10,16 @@ export function removeStyleById(id: string) {
   const removeElement = document.getElementById(id);
   if (removeElement != null) document.head.removeChild(removeElement);
 }
+
+export function insertElementStyleById(id: string, element: any, style: string) {
+  removeElementStyleById(id, element);
+  const insertStyle = document.createElement('style');
+  insertStyle.id = id;
+  insertStyle.innerHTML = style;
+  element.appendChild(insertStyle);
+}
+
+export function removeElementStyleById(id: string, element: any) {
+  const removeElement = document.getElementById(id);
+  if (removeElement != null) element.removeChild(removeElement);
+}
